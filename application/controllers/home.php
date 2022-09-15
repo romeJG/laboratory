d6f3d66a85ba9c40daedbb32da1ac7d28c894222
jastineee <201810083@fit.edu.ph>
2022-09-15 21:10:56 +0800

    routes and constructors

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,10 +2,16 @@
 
 class Home extends CI_Controller
 {
+
+    public function __construct()
+    {
+        //allways the first line
+        parent::__construct();
+    }
     // default lagi si index function
     public function index()
     {
-        //tatawagin si home_view
+        //tatawagin si home_view.php from the views folder and will load it on the page
         $this->load->view('home_view');
     }
 }
